d9ec3d7525c070e9694bedd847e2ab1136b8964c
nrrao <nandanarao@yahoo.com>
2019-11-20 16:46:08 -0500

    corrected typo changes

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABSE_URL
+  DATABASE_URL: process.env.DATABASE_URL
     || 'postgresql://dunder-mifflin@localhost/spaced-repetition',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL
